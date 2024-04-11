52bd7bd3030ce964b46d9109fcf4329ecf472f1b
Luka <138794790+BendUOverr@users.noreply.github.com>
2024-04-11 02:13:01 +0200

    feat:Login/Logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
@@ -18,11 +19,25 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 */
 
 Route::get('/', [DashboardController::class , 'index'])->name('ideas.index');
+
+
 Route::get('/ideas/{id}', [IdeaController::class , 'show'])->name('ideas.show');
 Route::post('/ideas', [IdeaController::class , 'store'])->name('ideas.store');
 Route::delete('/ideas/{id}', [IdeaController::class , 'destroy'])->name('ideas.destroy');
 Route::get('/ideas/edit/{id}', [IdeaController::class , 'edit'])->name('ideas.edit');
 Route::put('/ideas/{id}', [IdeaController::class , 'update'])->name('ideas.update');
+
+
 Route::post('/ideas/{id}/comments', [CommentController::class , 'store'])->name('ideas.comments.store');
+
+
+Route::get('/register', [AuthController::class , 'register'])->name('register');
+Route::post('/register', [AuthController::class , 'store']);
+Route::get('/login', [AuthController::class , 'login'])->name('login');
+Route::post('/login', [AuthController::class , 'authenticate']);
+Route::post('/logout', [AuthController::class , 'logout'])->name('logout');
+
+
+
 
 
